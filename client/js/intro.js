d35c18c372306ba9f076e8fe653a3d21a194bdda
Simon Chapman <eatyourpeasapps@gmail.com>
2017-05-28 12:37:02 +0100

    intro

--- client/js/intro.js
+++ client/js/intro.js
@@ -2,3 +2,9 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import '../templates/intro.html';
+
+Template.Intro.events({
+  'click #lets_get_started_button': function(event, template){
+    Router.go('disclaimer');
+  }
+})
