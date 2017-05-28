ec514e32d76d6a6db775883f6d3babe0b44e8ce4
Simon Chapman <eatyourpeasapps@gmail.com>
2017-05-28 12:15:17 +0100

    collection working

--- client/js/medication.js
+++ client/js/medication.js
@@ -2,3 +2,18 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import '../templates/medication.html';
+
+
+
+Template.Medication.onCreated(function bodyOnCreated() {
+
+  Meteor.subscribe('medications');
+
+
+});
+
+Template.Medication.helpers({
+  'medicines': function(){
+    return Medications.find().fetch();
+  }
+})
