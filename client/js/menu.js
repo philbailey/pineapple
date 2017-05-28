428a9c0fe562a137ca82ef9576f9313870377e8b
philbailey@github.com <pbgithub@pjbnet.com>
2017-05-28 11:03:23 +0100

    Add menu HTML/CSS and broken JS

--- client/js/menu.js
+++ client/js/menu.js
@@ -2,3 +2,16 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import '../templates/menu.html';
+
+Template.Activation.events({
+//  'click .top-left': function(event, template){
+//	console.log("top-left");
+//    event.preventDefault();
+//    Router.go("Diagnosis");
+//  },
+  'click #menu_form': function(event, template){
+	console.log("menu_form");
+    event.preventDefault();
+    Router.go("diagnosis");
+  }
+});
