44d6d71f4b708473045927a031137b6c1c38a2c6
Simon Chapman <eatyourpeasapps@gmail.com>
2017-05-30 02:13:25 +0100

    medication CRUD implemented

--- client/js/medication_item.js
+++ client/js/medication_item.js
@@ -5,5 +5,23 @@ import { ReactiveVar } from 'meteor/reactive-var';
 Template.Medication_Item.events({
   'click .return_button': function(event, template){
       Router.go('medication');
+  },
+  'submit #update_medication_form': function(event, template){
+    event.preventDefault();
+
+    var medicineName = event.target.medication_name.value;
+    var medicineFrequency = event.target.medication_frequency.value;
+    var medicineStart = event.target.medication_date.value;
+
+
+    Medications.update({_id: this._id},{
+      medication_name: medicineName,
+      medication_frequency: medicineFrequency,
+      medication_date: medicineStart,
+      medication_createdAt: new Date()
+    });
+
+    Router.go('medication');
   }
+
 });
